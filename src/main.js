46e3bd3af4bb45a7946d3159892951331a0431e9
littlewhitehhh <1021014610@qq.com>
2021-07-23 16:36:52 +0800

    add files

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/elementplugin.js'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false //关闭生产模式的提示
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
